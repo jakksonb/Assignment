26187b040ab3887fa4897257245ba63c0569f6a2
jakksonb <24221@haurakiplains.school.nz>
2026-08-04 13:40:42 +1200

    wall

--- scripts/gem.gd
+++ scripts/gem.gd
@@ -6,8 +6,15 @@ func _on_body_entered(body: Node2D) -> void:
 	#also add here something to change the player score
 	if body.is_in_group("player"):
 		Score.add_point(5)
-		Sprite.play("collect",1.0,false)  #replace frame time with a float of the time (seconds) per frame
-	if !Sprite.is_playing():  #wait for anim end
-		queue_free()
+		Sprite.play("collect",1.0,false)
+		print(Sprite.animation)
+		  #replace frame time with a float of the time (seconds) per frame
+	if not Sprite.is_playing():  #wait for anim end
+		print("anim fin")
+		#self.queue_free()
 		
 		
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	self.queue_free() # Replace with function body.
