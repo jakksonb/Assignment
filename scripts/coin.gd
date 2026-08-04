d070a6ab2c2f96e3c8222ff68b9d51b2d9c9dd21
jakksonb <24221@haurakiplains.school.nz>
2026-08-04 12:11:35 +1200

    i was fixing animations for coin and gem
    xavier helped me make a code for playing an animation when gem or coin is collected

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
+@onready var Sprite = $AnimatedSprite2D
 
-
-func _on_body_entered(body: Node) -> void:
-	body_entered.connect(_on_body_entered)
-	
-
-
+func _on_body_entered(body: Node2D) -> void:
+	#also add here something to change the player score
 	if body.is_in_group("player"):
-		Score.add_point(1)
-		queue_free()
+		Score.add_point(5)
+		Sprite.play("collect",1.0,false)  #replace frame time with a float of the time (seconds) per frame
+	if !Sprite.is_playing():  #wait for anim end
+		self.queue_free()
 
 		
--- scripts/gem.gd
+++ scripts/gem.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
-
+@onready var Sprite = $AnimatedSprite2D
 
 func _on_body_entered(body: Node2D) -> void:
-	$AnimationPlayer.play("collect")
-
+	#also add here something to change the player score
 	if body.is_in_group("player"):
 		Score.add_point(5)
+		Sprite.play("collect",1.0,false)  #replace frame time with a float of the time (seconds) per frame
+	if !Sprite.is_playing():  #wait for anim end
 		queue_free()
+		
 		
