60cfa1af402f7e9a1609078179e88b7788f62da2
jakksonb <24221@haurakiplains.school.nz>
2026-09-02 15:05:41 +1200

    add double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,16 +16,21 @@ const JUMP_VELOCITY = -300
 const jumppad_velocity = -450
 var health = 100
 var stamina = 100
+var jump_count = 0
+var max_jumps = 2
 
 func _physics_process(delta: float) -> void:
 	#handles gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if is_on_floor():
+		jump_count = 0
+	
 	#handles player jumping
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count += 1
 	# handles movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -6,6 +6,17 @@ extends Control
 
 #stamina and health
 func _process(delta: float) -> void:
+	if player.stamina == 100:
+		stamina.frame = 0
+	elif player.stamina == 75:
+		stamina.frame = 1
+	elif player.stamina == 50:
+		stamina.frame = 2
+	elif player.stamina == 25:
+		stamina.frame = 3
+	elif player.stamina == 0:
+		stamina.frame = 4
+		
 	if player.health == 100:
 		health.frame = 0
 	elif player.health == 75:
